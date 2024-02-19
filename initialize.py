182c18ba355775e1f8f611604200760e314b0b3e
bhroarsns <dixneufbhroarsns3mfcm@gmail.com>
2024-02-20 02:15:51 +0900

    bug fix

--- initialize.py
+++ initialize.py
@@ -1,7 +1,9 @@
 import os
 import json
 
-data = json.load(open("./config.json"))
+data = {}
+if os.path.isfile("./config.json"):
+    data = json.load(open("./config.json"))
 data["dir"] = os.getcwd()
 with open("./config.json", "w") as w:
     w.write(json.dumps(data, indent=4))
